105ddd465e80d5aa1684687170a0b144b98a46cb
siddastic <siddhantgaurav2003@gmail.com>
2022-10-26 04:16:06 +0530

    testing widget

--- src/ivy.ts
+++ src/ivy.ts
@@ -1,1 +1,7 @@
-// Initial Commit
+import { Widget } from "./api/widget";
+
+export class Ivy {
+    static wrapWidget(host: HTMLElement, widget: Widget) {
+        host.appendChild(widget.build());
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
+import { Key } from './api/key';
+import { StatelessWidget } from './api/stateless_widget';
+import { Ivy } from './ivy';
 import './style.css'
+
+const app = document.getElementById('app')!;
+
+class Text extends StatelessWidget{
+    constructor(public text: string){
+        super(Key.uniqueKey());
+    }
+
+    build(){
+        const element = document.createElement('div');
+        element.innerText = this.text;
+        return element;
+    }
+}
+
+Ivy.wrapWidget(app, new Text('Sample Text from text widget'));
